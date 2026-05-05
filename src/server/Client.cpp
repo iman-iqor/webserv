7fbeaafe03946e0d641a040ed92b50a6cfd397e5
Iman Iqor <iqorimane111@gmail.com>
2026-05-05 12:14:29 +0100

    added some modification redouane needs for the req parsing

--- src/server/Client.cpp
+++ src/server/Client.cpp
@@ -1,10 +1,11 @@
 #include "Client.hpp"
-Client::Client(int fd)
+Client::Client(int listen_fd, int client_fd, std::vector< ServerBlock* > *sv_block)
 {
-    this->fd = fd;
-    request_complete = false;
+    this->fd = client_fd;
+    this->listen_fd = listen_fd;
+    request.sv_blocks = sv_block;
     ready_to_send = false;
-    request = Request();
+    
     listen_fd = -1;
     // client_ip = "";
     // client_port = -1;
--- src/server/Socket.cpp
+++ src/server/Socket.cpp
@@ -28,6 +28,11 @@ void Server::setupSockets()
         //SOCK_STREAM indicates that it will be a TCP socket (as opposed to a datagram socket for UDP), and 0 means to use the default protocol for the given socket type (which is TCP for SOCK_STREAM). The return value is a file descriptor for the newly created socket, which can be used in subsequent system calls to configure and manage the socket. If the socket creation fails, it returns -1, which is checked in the code to throw an exception if the socket cannot be created successfully.
         if (sock < 0)
             throw std::runtime_error("Failed to create socket");
+        
+        //set the socket options to allow reuse of the address and port, which is useful for quickly restarting the server without waiting for the operating system to release the previous socket
+        int opt = 1;
+        if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+            throw std::runtime_error("Failed to set socket options");
 
         // find the port in listen directives and associate the socket with the corresponding server blocks
         for (size_t i = 0; i < config.servers.size(); i++)
