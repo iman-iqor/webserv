08ece071e2948c989179f2ce43aca6c1cb3ac40b
Iman Iqor <iqorimane111@gmail.com>
2026-06-05 17:42:01 +0100

    routind is fixed , i adde images directory inside www/, removed a non working main file and webserv.conf

--- src/Router/RouterGet.cpp
+++ src/Router/RouterGet.cpp
@@ -6,39 +6,36 @@ RouteInfo Router::routeGET(const Request &request, Location *location)
 
     std::string file_path = resolveFilePath(request.get_path(), location);
     std::cout << "resolved file path: " << file_path << std::endl;
-    if (!fileExists(file_path) && location->autoindex == false)
+    if (!fileExists(file_path))
     {
 
-        std::cout << "no file no dir" << std::endl;
         route_info.action = ERROR_404;
         route_info.http_status = 404;
         route_info.status_message = "Not Found";
         return route_info;
     }
 
-    if (isDirectory(file_path) || location->autoindex == true)
+    if (isDirectory(file_path))
     {
-        if (location->autoindex)
-        {
-            std::cout << "\033[31mautoindex enabled\033[31m" << std::endl;
-            route_info.action = DIRECTORY_LISTING;
-            route_info.file_path = file_path;
-            route_info.http_status = 200;
-            route_info.status_message = "OK";
-            return route_info;
-        }
-        std::cout << "is a directory" << std::endl;
         std::string index_path = file_path;
-        if (file_path[file_path.size() - 1] != '/')
+        if (index_path[index_path.size() - 1] != '/')
             index_path += "/";
-
         index_path += location->index;
-
         if (fileExists(index_path) && !isDirectory(index_path))
         {
             std::cout << "\033[31mindex file found\033[31m" << std::endl;
             route_info.action = SERVE_FILE;
             route_info.file_path = index_path;
+            route_info.http_status = 200;
+            route_info.status_message = "OK";
+            return route_info;
+        }
+
+        if (location->autoindex == true)
+        {
+            std::cout << "\033[31mautoindex enabled\033[31m" << std::endl;
+            route_info.action = DIRECTORY_LISTING;
+            route_info.file_path = file_path;
             route_info.http_status = 200;
             route_info.status_message = "OK";
             return route_info;
