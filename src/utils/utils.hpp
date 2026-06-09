d0041ba71c1852274178d90ce8717053da552d39
RadouaneAbn <redouane1997.ocs@gmail.com>
2026-06-09 20:15:10 +0100

    add debug printing function

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -2,7 +2,10 @@
 #define UTILS_HPP
 
 #include <sstream>
+#include <iostream>
 #include <string>
+#include <iomanip>
+#include <cctype>
 #include "../http/Exceptions.hpp"
 #include "../config/Config.hpp"
 
@@ -12,8 +15,8 @@ std::string& to_lower(std::string &s);
 std::string to_upper(const std::string &s);
 bool has_other(const std::string &s, std::string &chars);
 std::string ft_itoa(int n);
-
 bool method_is_valid(const std::string& method);
+void print_safe_string(const std::string& str);
 
 Location *find_location(ServerBlock *server_block, const std::string& path);
 
